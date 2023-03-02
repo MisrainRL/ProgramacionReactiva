c557d43196987c4f8c44c7740102f747efce1efc
MisrainRL <80868507+MisrainRL@users.noreply.github.com>
2023-03-02 12:47:00 -0600

    Implementación de Programación reactiva

---
+++ ProgramacionReactiva/Main.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ProgramacionReactiva
+
+{
+    public class MainExercise
+    {
+        static void Main(string[] args)
+        {
+            StudentRecords records = new StudentRecords();
+            records.StudentAdded += (sender, student) =>
+            {
+                //.WriteLine($"{student.Name} was added, he is {student.Age} years old and his email is {student.Email}");
+            };
+
+            records.AddStudent("Juan", 20, "juan@mail.com");
+            records.AddStudent("Maria", 18, "maria@mail.com");
+            records.AddStudent("Carlos", 22, "carlos@mail.com");
+            records.AddStudent("Miguel", 28, "m@mail.com");
+            records.AddStudent("Ana", 18, "a@mail.com");
+            records.AddStudent("JJ", 22, "jj@mail.com");
+            records.ShowStudents();
+            records.StudentDeleted += (sender, student) =>
+            {
+                Console.WriteLine($"{student.Name} fue eliminad@.");
+            };
+
+
+
+            records.DeleteStudent(new Student() { Name = "Maria", 
+                                    Age = 18, Email = "maria@mail.com" });
+
+            records.DeleteStudent(new Student()
+            {
+                Name = "Ana",
+                Age = 18,
+                Email = "a@mail.com"
+            });
+           // records.ShowStudents();
+        }
+    }
+}
--- ProgramacionReactiva/Program.cs
+++ ProgramacionReactiva/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ProgramacionReactiva
+{ 
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class StudentRecords
+    {
+        public event EventHandler<Student> StudentAdded;
+        public event EventHandler<Student> StudentDeleted;
+
+        private List<Student> students = new List<Student>();
+
+        public void AddStudent(string name, int age, string email)
+        {
+            Student student = new Student() { Name = name, Age = age, Email = email };
+            students.Add(student);
+            StudentAdded?.Invoke(this, student);
+        }
+
+        public void DeleteStudent(Student student)
+        {
+            students.Remove(student);
+            StudentDeleted?.Invoke(this, student);
+        }
+
+        public void ShowStudents()
+        {
+            students.ForEach((student) =>
+            {
+                Console.WriteLine($"Se agregó {student.Name}, " +
+                    $"que tiene {student.Age} años " +
+                    $"y su correo electrónico es {student.Email}");
+
+            });
+        }
+    }
+}
